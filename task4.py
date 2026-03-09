72c3de12ad01a6415937e6c33ec1284bfc6b5749
analializardi884-hub <analia.lizardi884@students.cmcss.net>
2026-03-09 10:18:51 -0500

    h

--- task4.py
+++ task4.py
@@ -1,6 +1,6 @@
-drawbridge_raised = True
-if drawbridge_raised:
-    outcome = "Doom: you had ran to the drawbridge but its still not down yet."
-else:
+drawbridge_raised = False
+if not drawbridge_raised:
     outcome = "Thunder: You had ran through the drawbridge just in time."
+else:
+    outcome = "Doom: you had ran to the drawbridge but its still not down yet."
 print(outcome)
